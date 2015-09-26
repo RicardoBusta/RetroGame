3615182a0071f6b84482b9797f0ad36275d3bf8b
bustamante <ricardobqueiroz@gmail.com>
2015-09-26 19:23:55 -0300

    Adding the hello world project files and the SDL lib to the repository.

---
+++ game/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+#include "SDL.h"
+
+int main(int argc, char * argv[])
+{
+//  if (SDL_Init(SDL_INIT_VIDEO) != 0){
+//    std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+//    return 1;
+//  }
+
+  std::cout << "Hello World!";
+
+  return 0;
+}
+
